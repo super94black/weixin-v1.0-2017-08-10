596c396cfffb1e7fb9e030d7cdcf243148a379d9
super94black <931105779@qq.com>
2017-11-12 17:48:55 +0800

    Update Const.java

--- src/main/java/cn/zhanghl/util/Const.java
+++ src/main/java/cn/zhanghl/util/Const.java
@@ -1,14 +1,7 @@
 package cn.zhanghl.util;
 
 public class Const {
-	//测试号
-	public static final String AppId = "wxa7f2b2ab77d13411";//测试号
-	public static final String AppSecret = "4ec9788ffaccf3ca6f854598947aedf1";
-	//订阅号
-//	public static final String AppId = "wx5640dd2066d9953d";
-//	public static final String AppSecret = "6bc802f771368dac4305b0a11fc32d5a";
-	public static final String Token = "zhanghanlin";
-	public static final String EncodingAESKey = "AxRQekjLvotQZkBu6Xmf1x9q1qcQYtqmJw0egyJCT0k";
+
 	public static final String OPEN_ID = "OPEN_ID";
 	public static final int EXPRIES_TIME = 1800;
 	public static final String BUTTON_ONE_ONE = "11";
